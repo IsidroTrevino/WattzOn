bc4a8c8e9276d8974f6d65b23ebcaa8104de6489
IsidroTrevino <A00837104@tec.mx>
2024-10-11 11:59:13 -0600

    Initial Commit

---
+++ WattzOn/WattzOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  WattzOnApp.swift
+//  WattzOn
+//
+//  Created by Isidro Treviño on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WattzOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
